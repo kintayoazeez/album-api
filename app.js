7a56dd7ed019d93987bf5c613177effdfbe71713
ag2436 <ag2436@student.jamk.fi>
2026-04-21 04:11:40 +0300

    Fix app.js for production

--- app.js
+++ app.js
@@ -12,19 +12,15 @@ app.use(express.json())
 app.use('/api/albums', albumRoutes)
 
 // Connect to MongoDB and start server
-export async function startServer() {
+async function startServer() {
   await mongoose.connect(process.env.MONGODB_URI)
   console.log('Connected to MongoDB!')
   const PORT = process.env.PORT || 3000
-  const server = app.listen(PORT, () => {
+  app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`)
   })
-  return server
 }
 
-// Only start server if this file is run directly
-if (process.env.NODE_ENV !== 'test') {
-  startServer()
-}
+startServer()
 
 export default app
